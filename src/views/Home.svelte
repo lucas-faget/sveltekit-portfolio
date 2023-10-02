54116924fe780ea80cac2d5669a257a4a5f6c15c
lucas-faget <lucas.faget@laposte.net>
2023-10-02 17:03:42 +0200

    Update technology stack

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -60,7 +60,7 @@
                         { rating: Rating.ThreeStars, src: Assets.Logo + "typescript.svg", text: "TypeScript" },
                         { rating: Rating.TwoStars, src: Assets.Logo + "vue.svg", text: "Vue.js" },
                         { rating: Rating.TwoStars, src: Assets.Logo + "svelte.svg", text: "Svelte" },
-                        { rating: Rating.TwoStars, src: Assets.Logo + "react.svg", text: "React" }
+                        { rating: Rating.OneStar, src: Assets.Logo + "react.svg", text: "React" }
                     ]} />
                 </FromDownAnimation>
 
@@ -99,6 +99,7 @@
 
                 <FromDownAnimation>
                     <ListWithImageAndRating items={[
+                        { rating: Rating.ThreeStars, src: Assets.Logo + "git.svg", text: "Git" },
                         { rating: Rating.TwoStars, src: Assets.Logo + "linux.svg", text: "Linux" },
                         { rating: Rating.OneStar, src: Assets.Logo + "docker.svg", text: "Docker" }
                     ]} />
