868cba86f44ecea11c9d05e8a370a929b400f0b5
lucas-faget <lucas.faget@laposte.net>
2024-06-26 20:51:01 +0200

    Remove .netlify directory

--- .netlify/server/entries/pages/_page.svelte.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component, v as validate_component } from "../../chunks/ssr.js";
-import { W as Welcome } from "../../chunks/Welcome.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `${$$result.head += `<!-- HEAD_svelte-1may79_START -->${$$result.title = `<title>Home</title>`, ""}<meta name="description" content="Home"><!-- HEAD_svelte-1may79_END -->`, ""} <div>${validate_component(Welcome, "Welcome").$$render($$result, { text: "Home" }, {}, {})}</div>`;
-});
-export {
-  Page as default
-};
--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BAqISSEC.js","_app/immutable/chunks/scheduler.DWVu45ey.js","_app/immutable/chunks/index.5wTyoJXJ.js","_app/immutable/chunks/stores.WT1BNstW.js","_app/immutable/chunks/entry.tgNn17xH.js"];
-export const stylesheets = [];
-export const fonts = [];
