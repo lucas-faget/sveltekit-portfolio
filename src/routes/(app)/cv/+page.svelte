1ff33cb195e5ee58911539f44627303f6281e681
lucas-faget <lucas.faget@laposte.net>
2025-02-08 17:40:17 +0100

    Using PDF.js to display CV and adding a print button

--- src/routes/(app)/cv/+page.svelte
+++ src/routes/(app)/cv/+page.svelte
@@ -1,10 +1,48 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import type { PageData } from "./$types";
+    import * as pdfjsLib from "pdfjs-dist";
+    import pdfWorker from "pdfjs-dist/build/pdf.worker?url";
     import Chapter from "$lib/components/Chapter.svelte";
     import Title from "$lib/components/Title.svelte";
     import Button from "$lib/components/Button.svelte";
 
     export let data: PageData;
+
+    let canvas: HTMLCanvasElement | null = null;
+
+    const pdfUrl: string = data.cvFileName;
+
+    pdfjsLib.GlobalWorkerOptions.workerSrc = "/node_modules/pdfjs-dist/build/pdf.worker.min.mjs";
+
+    onMount(async () => {
+        if (canvas) {
+            const pdf = await pdfjsLib.getDocument(pdfUrl).promise;
+            const cv = await pdf.getPage(1);
+    
+            const scale: number = 1.5;
+            const viewport = cv.getViewport({ scale });
+    
+            const context = canvas.getContext("2d");
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
+    
+            await cv.render({
+                canvasContext: context,
+                viewport
+            });
+        }
+    });
+
+    const printCV = (): void => {
+        const printWindow = window.open(pdfUrl, "_blank");
+
+        if (printWindow) {
+            printWindow.onload = () => {
+                printWindow.print();
+            };
+        }
+    }
 </script>
 
 <svelte:head>
@@ -16,14 +54,15 @@
     <Chapter>
         <Title title={"Curriculum\nvitae"} />
 
-        <a href="{data.assetsBaseUrl}/{data.cvFileName}" download>
-            <Button rounded>Télécharger</Button>
-        </a>
+        <div class="flex flex-wrap gap-4">
+            <a href="{pdfUrl}" download>
+                <Button rounded>Télécharger</Button>
+            </a>
+            <button on:click={printCV}>
+                <Button rounded>Imprimer</Button>
+            </button>
+        </div>
 
-        <embed
-            src="{data.assetsBaseUrl}/{data.cvFileName}"
-            type="application/pdf"
-            class="w-full max-w-[210mm] aspect-[210/297]"
-        />
+        <canvas bind:this={canvas} class="w-full max-w-[210mm]"></canvas>
     </Chapter>
 </div>
